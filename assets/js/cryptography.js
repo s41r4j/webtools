874834fd4a7c9a1838be73957aa9ca77e9d6c445
S41R4J <65067289+s41r4j@users.noreply.github.com>
2023-08-04 20:21:01 +0530

    Update cryptography.js

--- assets/js/cryptography.js
+++ assets/js/cryptography.js
@@ -1,121 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const inputTextElement = document.getElementById('inputText');
-    const outputTextElement = document.getElementById('outputText');
-    const cryptoOptionElement = document.getElementById('cryptoOption');
-    const copyButton = document.getElementById('copyButton');
-    const passwordToggle = document.getElementById('passwordToggle');
-    const popUpMessage = document.getElementById('popUpMessage');
-  
-    function showMessage() {
-      popUpMessage.classList.add('show');
-      setTimeout(() => {
-        popUpMessage.classList.remove('show');
-      }, 2000);
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    passwordToggle.addEventListener('click', function() {
-      if (passwordToggle.checked) {
-        console.log('checked');
-        inputTextElement.setAttribute('style', 'color: transparent;text-shadow: 0 0 8px rgba(0,0,0,0.5);');
-      } else {
-        inputTextElement.removeAttribute('style');
-      }
-    });
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/cryptography.css">
+  <title>[ Cryptography ]</title>
+</head>
 
-    copyButton.addEventListener('click', function() {
-      outputTextElement.select();
-      document.execCommand('copy');
-      showMessage();
-    });
-  
-    let previousInput = '';
-    let previousOutput = '';
-  
-    function md5(text) {
-      return CryptoJS.MD5(text).toString();
-    }
-  
-    function sha1(text) {
-      return CryptoJS.SHA1(text).toString();
-    }
-  
-    function sha256(text) {
-      return CryptoJS.SHA256(text).toString();
-    }
-  
-    function sha512(text) {
-      return CryptoJS.SHA512(text).toString();
-    }
-  
-    function base64Encode(text) {
-      return btoa(text);
-    }
-  
-    function base64Decode(encodedText) {
-      return atob(encodedText);
-    }
-  
-    function rot13(text) {
-      return text.replace(/[a-zA-Z]/g, function(c) {
-        const charCode = c.charCodeAt(0);
-        const baseCharCode = c >= 'a' ? 'a'.charCodeAt(0) : 'A'.charCodeAt(0);
-        return String.fromCharCode((charCode - baseCharCode + 13) % 26 + baseCharCode);
-      });
-    }
-  
-    function updateOutput() {
-      const inputText = inputTextElement.value;
-      const selectedOption = cryptoOptionElement.value;
-  
-      if (inputText === previousInput) {
-        outputTextElement.value = previousOutput;
-        return;
-      }
-  
-      let outputText = '';
-      switch (selectedOption) {
-        case 'md5':
-          outputText = md5(inputText);
-          break;
-        case 'sha1':
-          outputText = sha1(inputText);
-          break;
-        case 'sha256':
-          outputText = sha256(inputText);
-          break;
-        case 'sha512':
-          outputText = sha512(inputText);
-          break;
-        case 'base64':
-          outputText = base64Encode(inputText);
-          break;
-        case 'base64Decode':
-          outputText = base64Decode(inputText);
-          break;
-        case 'rot13':
-          outputText = rot13(inputText);
-          break;
-        default:
-          outputText = '';
-      }
-  
-      outputTextElement.value = outputText;
-      previousInput = inputText;
-      previousOutput = outputText;
-    }
-  
-    function clearOutput() {
-      outputTextElement.value = '';
-      previousInput = '';
-      previousOutput = '';
-    }
-  
-    inputTextElement.addEventListener('input', updateOutput);
-    cryptoOptionElement.addEventListener('change', function() {
-      clearOutput();
-      updateOutput();
-    });
+<body>
+  <div class="container">
+    <h1 class="heading"><span class="initials">C</span>ryptography</h1>
+    <form id="cryptoForm">
+      <label for="inputText">Input:</label>
+      <div class="input-container">
+        <textarea id="inputText" placeholder="Enter your text..."></textarea>
+      </div>
 
-    inputTextElement.focus();
-  });
-  
+      <label for="inputSalt" id="saltLabel">Salt:</label>
+      <div class="input-container">
+        <textarea id="inputSalt" placeholder="Enter your salt..."></textarea>
+      </div>
+
+      <label for="cryptoOption">Select an Encryption:</label>
+      <select id="cryptoOption">
+        <option value="md5">MD5</option>
+        <option value="sha1">SHA-1</option>
+        <option value="sha256">SHA-256</option>
+        <option value="sha512">SHA-512</option>
+        <option value="zcrypt">Zcrypt</option>
+        <option value="base64">Base64 Encode</option>
+        <option value="base64Decode">Base64 Decode</option>
+        <option value="rot13">ROT13</option>
+      </select>
+
+      <label for="outputText">Output:</label>
+      <textarea id="outputText" placeholder="Output will appear here..." readonly></textarea>
+
+      <button type="button" id="copyButton">Copy</button>
+
+      <div class="toggle-container">
+        <input type="checkbox" class="checkbox" id="passwordToggle">
+        <label class="toggle" for="passwordToggle">
+          <div class="slider"></div>
+        </label>
+      </div>
+    </form>
+
+    <div id="popUpMessage">Copied!</div>
+  </div>
+
+  <script src="../assets/js/crypto-js.min.js"></script>
+  <script src="../assets/js/zcrypt.js"></script>
+  <script src="../assets/js/cryptography.js"></script>
+</body>
+
+</html>
